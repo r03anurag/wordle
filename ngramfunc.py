143b965313a51007f38f6b197f004f6ec950782f
Anurag Renduchintala <hh3232@wayne.edu>
2025-02-09 01:19:26 -0500

    backend complete

---
+++ ngramfunc.py
@@ -0,0 +1,50 @@
+########## file responsible for reading the english words dictionary, and training an N-Gram (bigram)
+#          character-level model on it with probabilities
+########## Word list credits: https://github.com/dwyl/english-words/tree/master
+from nltk.util import ngrams
+from collections import Counter
+from math import log, inf
+import random
+
+'''Function that reads dictionary and collects counts'''
+def collect_all_words_and_counts(consideration_length: int):
+    datapath = "data/words_alpha.txt"
+    words = []
+    bigram_counts = Counter()
+    unigram_counts = Counter()
+    with open(datapath, 'r') as wordfile:
+        for word in wordfile:
+            word = word.replace("\n","")
+            lc = len(word) == consideration_length
+            if lc:
+                wu = word.upper()
+                ng = list(ngrams(sequence=wu, n=2))
+                bigram_counts.update(ng)
+                unigram_counts.update(wu)
+                words.append(wu)
+    return bigram_counts, unigram_counts, words
+
+'''Function that calculates probability of a sequence'''
+def calculate_sequence_log_probability(seq: str, bigram_counts: dict, unigram_counts: dict):
+    '''Short Function that calculates Add-1 MLE (log-prob):
+        P_Add-1(wi|w(i-1)) = ln([c(w(i-1), wi) + 1]/[c(w(i-1)) + 26])
+    '''
+    bigram_log_prob = lambda c1, c2: log((bigram_counts[(c1,c2)]+1)/(unigram_counts[c1]+26))
+    if len(seq) >= 2:
+        ng2 = list(ngrams(sequence=seq, n=2))
+        lprob = 0
+        for (c1,c2) in ng2:
+            lprob += bigram_log_prob(c1,c2)
+        return lprob
+    return inf
+
+'''Main function. Run this.'''
+def get_words_and_seed(desired_length: int = 5):
+    bigramCount, unigramCount, wordList = collect_all_words_and_counts(consideration_length=desired_length)
+    wordList = sorted(wordList, key=lambda rw: calculate_sequence_log_probability(seq=rw, 
+                                                                               bigram_counts=bigramCount, 
+                                                                               unigram_counts=unigramCount))
+    # pick a seed from the 20 most common words of that length
+    seed = random.choice(wordList[-20:])
+    wordList.remove(seed)
+    return wordList+[seed]
---
+++ wordle.py
@@ -0,0 +1,131 @@
+####### file that implements necessary function for wordle gameplay
+from math import ceil
+from collections import Counter
+import string
+import ngramfunc
+import positional
+
+'''Class that represents a human Wordle player's game'''
+class HumanPlayerWordle:
+    # constructor
+    # self.answer -> the answer for this round (str)
+    # self.progress -> attempts (list of strings) made so far, and information that
+    #                  indicates which letters don't exist (~), are misplaced (*), and are correctly placed (+).
+    #                  Example: A+U~D~I*O* =>
+    #                       - A is correctly placed
+    #                       - I, O are misplaced
+    #                       - U, D don't exist in the word
+    # self.attempts -> how many tries are left. initial num. of attempts = ceil(word_length*1.2)
+    # self.counts -> counts of each letter in the answer.
+    # self.used -> which letters have been used? Dict mapping each letter to a boolean.
+    def __init__(self, answer: str):            
+        self.answer = answer
+        self.progress = []
+        self.attempts = ceil(len(answer)*1.2)
+        self.counts = Counter(answer)
+        self.used = dict()
+        for ul in string.ascii_uppercase:
+            self.used[ul] = False
+
+    # helper function to calculate margin of error for *
+    def calculate_margin_of_error(self, guess: str):
+        corrects = Counter()
+        for h in range(len(guess)):
+            corrects[self.answer[h]] += int(self.answer[h] == guess[h])
+        return self.counts - corrects
+    
+    # function that evaluates a guess made
+    def evaluate_guess(self, guess: str):
+        if self.attempts > 0:
+            if guess == self.answer:
+                return self.answer
+            info = ["" for _ in range(len(self.answer))]
+            margin = self.calculate_margin_of_error(guess=guess)
+            for i in range(len(guess)):
+                if guess[i] == self.answer[i]:
+                    info[i] = guess[i]+"+"
+                elif guess[i] in self.counts:
+                    if margin[guess[i]] > 0:
+                        info[i] = guess[i]+"*"
+                        margin[guess[i]] = max((0,margin[guess[i]]-1))
+                    else:
+                        info[i] = guess[i]+"~"
+                else:
+                    info[i] = guess[i]+"~"
+                self.used[guess[i]] = True
+            self.attempts -= 1
+            infoStr = "".join(info)
+            self.progress.append(infoStr)
+            return infoStr
+        # (!) indicates that answer is returned, but because attempts are over
+        return "!"+self.answer
+
+
+'''Class that reprsents a computer's wordle game'''
+class ComputerPlayerWordle:
+    # constructor
+    # self.progress -> attempts (list of strings) made so far, and information that
+    #                  indicates which letters don't exist (~), are misplaced (*), and are correctly placed (+).
+    #                  Example: A+U~D~I*O* =>
+    #                       - A is correctly placed
+    #                       - I, O are misplaced
+    #                       - U, D don't exist in the word
+    # self.word_length = how long is the word? At most 10.
+    # self.attempts -> how many tries are left. initial num. of attempts = ceil(word_length*1.2)
+    # self.heuristic -> how to score words. [0: bigram-log-probabilities (default), 1: positional-scores]
+    # self.words -> list of dictionary words sorted by their log-probabilities/positional-scores in ascending order.
+    def __init__(self, wordlen: int, heur: int = 0):
+        self.progress = []
+        self.word_length = wordlen
+        self.attempts = ceil(wordlen*1.2)
+        self.heuristic = heur
+        self.words = ngramfunc.get_words_and_seed(desired_length=wordlen) if self.heuristic == 0 \
+                        else positional.get_words_and_seed(desired_length=wordlen)
+    
+    # function to get user feedback for a guess. Only needed if using command-line
+    def get_user_feedback_CLI(self, guess: str):
+        fbs = input(f"My guess is {guess}. Evaluate it: ")
+        return fbs
+    
+    # helper function to get letter statuses (conditions) from feedback string (Don't directly call)
+    def get_conditions(self, feedback: str):
+        crt = set()
+        mis = set()
+        crt_cond = []
+        mis_cond = []
+        inc_cond = []
+        for k in range(0, len(feedback), 2):
+            if feedback[k+1] == "+":
+                crt.add(feedback[k])
+            elif feedback[k+1] == "*":
+                mis.add(feedback[k])
+        for j in range(0, len(feedback), 2):
+            if feedback[j+1] == "+":
+                crt_cond.append(f"word[{j//2}] == '{feedback[j]}'")
+            elif feedback[j+1] == "*":
+                mis_cond.append(f"word[{j//2}] != '{feedback[j]}' and '{feedback[j]}' in word")
+            else:
+                if feedback[j] not in crt and feedback[j] not in mis:
+                    inc_cond.append(f"'{feedback[j]}' not in word")
+        return " and ".join(crt_cond+mis_cond+inc_cond)
+
+    # function to process the generated conditions
+    def process_conditions(self, feedback: str):
+        cond = self.get_conditions(feedback=feedback)
+        self.words = list(filter(lambda word: eval(cond), self.words))
+
+    # main process (making guess and incorporating the feedback)
+    def run(self):
+        while True:
+            if len(self.words) == 0:
+                print("Based on the given feedback, there are no words that match your criteria. Please try again.")
+                break
+            guess = self.words.pop()
+            fb = self.get_user_feedback_CLI(guess=guess)
+            self.progress.append(fb)
+            if fb.count("+") == self.word_length:
+                break
+            _ = self.process_conditions(fb)
+
+cw = ComputerPlayerWordle(wordlen=5, heur=1)
+cw.run()
