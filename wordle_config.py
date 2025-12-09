b0de3d153b4f248ac21dcbf0229fb532f9579954
Anurag Renduchintala <hh3232@wayne.edu>
2025-12-09 04:35:24 -0500

    minor change in gitignore (see readme for details)

--- wordle_config.py
+++
@@ -1,7 +0,0 @@
-############# SHORT FILE WITH CONFIGURATION PARAMETERS. CHANGE TO YOUR LIKING
-WORDLENGTH=6    # must be between 5-8
-COMPUTER_MODE=False # must be true/false
-HEURISTIC=1     # must be either 0 (NGram probability), 1 (positional letter count)
-COMMON_ONLY=True  # use only well-known words? Default true. If false, all words are used.
-SCOPE=40 # when picking a seed, how many top choices of words should be considered? Must be at least 1. Default 20.
-UNIQUE=True # prioritize words that contain unique letters?
